24017b531360f2aaaa87a059e7ec1df849548cb0
Uday kiran <76544318+Udaykirank2611@users.noreply.github.com>
2024-08-07 20:14:01 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,8 +7,11 @@ branch = st.selectbox("Select Your Branch :",
 ["CSE","DS","AIML","IOT","IT","ECE"])
 paper = st.selectbox("Examination : ",
 ["Mid1","Mid2","Sem"])
+pdf_url = "https://drive.google.com/file/d/1KQ3O-nq8hCe1kwa16HSRrHhMI4pHMIrH/preview"
 if(st.button("Submit")):
-    st.text("hiii")
-    pdf_url = "https://drive.google.com/file/d/1KQ3O-nq8hCe1kwa16HSRrHhMI4pHMIrH/preview"
+    if(year=="1st Year"):
+        if(branch=="DS"):
+            if(paper=="Mid1"):
+                pdf_url = "https://drive.google.com/file/d/1KQ3O-nq8hCe1kwa16HSRrHhMI4pHMIrH/preview"
     pdf_display = f'<iframe src="{pdf_url}" width="700" height="1000" type="application/pdf"></iframe>'
     st.markdown(pdf_display, unsafe_allow_html=True)
