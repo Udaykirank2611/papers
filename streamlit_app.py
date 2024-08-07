4906ae99550430e6b6690121b6c469621a53e94c
Uday kiran <76544318+Udaykirank2611@users.noreply.github.com>
2024-08-07 20:21:36 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,5 +13,7 @@ if(st.button("Submit")):
         if(branch=="DS"):
             if(paper=="Mid1"):
                 pdf_url = "https://drive.google.com/file/d/1KQ3O-nq8hCe1kwa16HSRrHhMI4pHMIrH/preview"
+            if(paper=="Mid2"):
+                pdf_url = "https://drive.google.com/file/d/1JcZWHKk3aLVZjHO6fwTYyhiZtN1RdeTd/preview"
     pdf_display = f'<iframe src="{pdf_url}" width="700" height="1000" type="application/pdf"></iframe>'
     st.markdown(pdf_display, unsafe_allow_html=True)
