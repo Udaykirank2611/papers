f5f4a751dcdefe9bead86a8a5ab54968206e0717
Uday kiran <76544318+Udaykirank2611@users.noreply.github.com>
2024-08-07 21:31:43 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,8 @@
 import streamlit as st
 
 st.title("MVSREC AUTONOMOUS PAPERS")
-year = st.selectbox("Select Your Year : ",
-["1st Year","2nd Year","3rd Year"])
+year = st.selectbox("Select Your Year and Sem : ",
+["1st Year 1st Sem","1st Year 2nd Sem","2nd Year 1st Sem","2nd Year 2nd Sem","3rd Year 1st Sem","3rd Year 2nd Sem"])
 if(year=="3rd Year"):
     branch = st.selectbox("Select Your Branch :",
 ["CSE","IT","ECE"])
@@ -10,22 +10,22 @@ else:
     branch = st.selectbox("Select Your Branch :",
 ["CSE","DS","AIML","IOT","IT","ECE"])
 paper = st.selectbox("Examination : ",
-["Sem1 Mid1","Sem1 Mid2","Sem1","Sem2 Mid1","Sem2 Mid2","Sem2"])
+["Mid1","Mid2","Sem"])
 pdf_url = None
 if(st.button("Submit")):
-    if(year=="1st Year"):
+    if(year=="1st Year 1st Sem"):
         if(branch=="DS"):
-            if(paper=="Sem1 Mid1"):
+            if(paper=="Mid1"):
                 pdf_url = "https://drive.google.com/file/d/1KQ3O-nq8hCe1kwa16HSRrHhMI4pHMIrH/preview"
-            if(paper=="Sem1 Mid2"):
+            if(paper=="Mid2"):
                 pdf_url = "https://drive.google.com/file/d/1JcZWHKk3aLVZjHO6fwTYyhiZtN1RdeTd/preview"
-    if(year=="2nd Year"):
+    if(year=="2nd Year 2nd Sem"):
         if(branch=="CSE"):
-            if(paper=="Sem 2 Mid1"):
+            if(paper=="Mid1"):
                 pdf_url = "https://drive.google.com/file/d/1q27z5x7oK92hC9bb_mB-InUkZDZESkPz/preview"
-            elif(paper=="Sem 2 Mid2"):
+            elif(paper=="Mid2"):
                 pdf_url = "https://drive.google.com/file/d/1jnq_nQwKDOOgpZauapr5xaDAVC6h5n59/preview"
-            elif(paper=="Sem2"):
+            elif(paper=="Sem"):
                 pdf_url = "https://drive.google.com/file/d/1R8fejWceCxq9sR4TP9hcYeEbYH7G9L6Y/preview"
     if(pdf_url=="None"):
         st.text("WILL BE UPDATED SOON")
