467dbfda5fe456d9a6bdc81dd90ff13e52cc0c55
Uday kiran <76544318+Udaykirank2611@users.noreply.github.com>
2024-08-07 19:54:55 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,13 @@
 import streamlit as st
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+st.title("MVSREC AUTONOMOUS PAPERS")
+year = st.selectbox("Select Your Year : ",
+["1st Year","2nd Year","3rd Year"])
+branch = st.selectbox("Select Your Branch :",
+["CSE","DS","AIML","IOT","IT","ECE"])
+paper = st.selectbox("Examination : ",
+["Mid1","Mid2","Sem"])
+if(st.button("Submit")):
+    st.text("hiii")
+    pdf_display = f'<iframe src="{"https://drive.google.com/file/d/1KQ3O-nq8hCe1kwa16HSRrHhMI4pHMIrH/view?usp=drive_link"}" width="700" height="1000" type="application/pdf"></iframe>'
+    st.markdown(pdf_display, unsafe_allow_html=True)
